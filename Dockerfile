e37d0c4bb8b42f323442d43fcb8a6f0f4c2f1e8e
hanl1946@163.com <hanl1946@163.com>
2018-11-16 19:06:29 +0800

    首次提交

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM tomcat
+COPY build/docker/**.war /usr/local/tomcat/webapps/
---
+++ src/main/java/net/le/minzhile_shop/MinzhileShopApplication.java
@@ -0,0 +1,12 @@
+package net.le.minzhile_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinzhileShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinzhileShopApplication.class, args);
+    }
+}
